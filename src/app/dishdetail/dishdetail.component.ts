7e50438a8273bc403962967b9941695da1aed872
mhornstein <mor.h100@gmail.com>
2018-12-07 22:38:23 +0200

    SPA

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { digest } from '@angular/compiler/src/i18n/serializers/xmb';
 import { Dish } from '../shared/dish';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { DishService } from '../services/dish.service';
+
 
 @Component({
   selector: 'app-dishdetail',
@@ -11,13 +16,19 @@ import { Dish } from '../shared/dish';
 
 
 export class DishdetailComponent implements OnInit {
-
-  @Input()
   dish: Dish;
 
-  constructor() { }
+  constructor( private dishService: DishService,
+    private location: Location, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    let id = this.route.snapshot.params['id'];
+    this.dish = this.dishService.getDish(id);
+    debugger
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
