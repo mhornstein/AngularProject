8955171037124fa95427560c689ef9355bdf541f
mhornstein <mor.h100@gmail.com>
2018-12-27 16:10:38 +0200

    Adding the prev and next in dishdetail.component

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -5,6 +5,7 @@ import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {MatSliderModule} from '@angular/material/slider';
+import { switchMap } from 'rxjs/operators';
 
 import { Comment } from '../shared/comment';
 import { DishService } from '../services/dish.service';
@@ -21,6 +22,11 @@ export class DishdetailComponent implements OnInit {
   commentForm: FormGroup;
   @ViewChild('fform') commentFormDirective;
   errMess: string;
+
+  dishIds: string[];
+  prev: string;
+  next: string;
+
 
   formErrors = {
     'author': '',
@@ -52,9 +58,9 @@ export class DishdetailComponent implements OnInit {
     }
 
   ngOnInit() {
-    const id = this.route.snapshot.params['id'];
-    this.dishService.getDish(id).subscribe(dish => {this.dish = dish;
-      this.dishCopy = dish; }, errmes => this.errMess = <any>errmes);
+    this.dishService.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
+    this.route.params.pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
+    .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
   }
 
   goBack(): void {
@@ -105,5 +111,11 @@ export class DishdetailComponent implements OnInit {
       .subscribe(dish => {
         this.dish = dish; this.dishCopy = dish; },
         errmess => {this.dish  = null; this.dishCopy = null; this.errMess = <any>errmess; } );
+  }
+
+  setPrevNext(dishId: string) {
+    const index = this.dishIds.indexOf(dishId);
+    this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
+    this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
   }
 }
